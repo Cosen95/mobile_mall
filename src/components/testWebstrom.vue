10730d9fa612bba9ff32e525731464bf8d466859
fengshuan <1263215592@qq.com>
2018-07-14 11:35:52 +0800

    测试webstrom连接github

---
+++ src/components/testWebstrom.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="test">测试webstrom连接github</div>
+</template>
+
+<script>
+    export default {
+        name: "testWebstrom"
+    }
+</script>
+
+<style scoped>
+  .test{
+    font-size: 20px;
+    color:skyblue;
+  }
+</style>
